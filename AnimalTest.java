b49770919029d3e47e2d821499187aab290f973d
Kunishy <149185650+Kunishy@users.noreply.github.com>
2024-11-06 06:03:58 +0800

    feat: version2

---
+++ AnimalTest.java
@@ -0,0 +1,50 @@
+public class AnimalTest{
+    public static void main(String args[]){
+        PolymorphismDemo demo = new PolymorphismDemo();
+        demo.PolymorphismUsingFor();
+        Dog dog = new Dog(); //如果換成Cat Dog = new Cat(); 結果會不一樣
+        demo.lyric(dog);
+    }
+}
+
+abstract class Animal {
+    public abstract String bark();
+}
+
+class Dog extends Animal{
+    @Override 
+    public String bark(){
+        return "汪汪!";
+    }
+}
+
+class Cat extends Animal{
+    @Override 
+    public String bark(){
+        return "喵 ~ !";
+    }
+}
+
+class Cow extends Animal{
+    @Override 
+    public String bark(){
+        return "哞 ~ !";
+    }
+}
+
+class PolymorphismDemo{
+    public void PolymorphismUsingFor(){
+        Animal[] animals = new Animal[3];
+        animals [0] = new Dog();
+        animals [1] = new Cat();
+        animals [2] = new Cow();
+
+        for (int i=0;i<animals.length; i++){
+            System.out.println(animals[i].bark());
+        }
+    }
+
+    public void lyric(Animal animal){
+        System.out.println("隔壁傳來叫聲:"+animal.bark());
+    }
+}
