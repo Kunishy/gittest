df024c69d20d6e7acd0ee578078c7d2802643f88
Kunishy <149185650+Kunishy@users.noreply.github.com>
2024-11-06 06:48:58 +0800

    feat: add annotation

--- AnimalTest.java
+++ AnimalTest.java
@@ -2,7 +2,7 @@ public class AnimalTest{
     public static void main(String args[]){
         PolymorphismDemo demo = new PolymorphismDemo();
         demo.PolymorphismUsingFor();
-        Cat dog = new Cat(); //如果換成Cat Dog = new Cat(); 結果會不一樣
+        Cat dog = new Cat(); //fajok,
         demo.lyric(dog);
     }
 }
